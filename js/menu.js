01d50a466da36e5968cf313df5b9bd11458e5a68
Mong'are <Mongaresams@gmail.com>
2021-11-18 19:21:20 +0300

    Fix the linters errors present

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,9 @@
-const menuBtn = document.getElementById("menu-btn");
-const links = document.getElementById("links");
-
-
-menuBtn.addEventListener("click", displayDate);
+const menuBtn = document.getElementById('menu-btn');
+const links = document.getElementById('links');
 
 function displayDate() {
-  menuBtn.classList.toggle("open");
-  links.classList.toggle("open");
+  menuBtn.classList.toggle('open');
+  links.classList.toggle('open');
 }
+
+menuBtn.addEventListener('click', displayDate);
